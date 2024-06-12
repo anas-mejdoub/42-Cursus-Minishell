7de67abb4d25405a6943f639a09185c6aab5eaad
nourddine-benyahya <nourddine.benyahya02@gmail.com>
2024-06-12 15:55:07 +0100

    add some flags for readline and err push for create new branch

--- sources/lexer/lexer.c
+++ sources/lexer/lexer.c
@@ -6,8 +6,27 @@
 /*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 15:12:28 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/06/06 15:12:53 by nbenyahy         ###   ########.fr       */
+/*   Updated: 2024/06/12 15:54:00 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lexer.h"
+#include "../../includes/minishell.h"
+
+
+
+void lexer()
+{
+    while (1)
+    {
+        char* line = readline("tchbi7a-shell$");
+        if (line) {
+            printf("tchbi7a-shell$%s\n", line);
+            free(line);
+        }
+    }
+}
+
+int main() {
+    lexer();
+    return 0;
+}
