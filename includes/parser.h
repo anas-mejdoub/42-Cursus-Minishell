87b0747638cc1e0642e684b2d79be72b853c2ef5
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-06-14 16:09:28 +0100

    trees headrer file

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,34 @@
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 11:01:25 by amejdoub          #+#    #+#             */
-/*   Updated: 2024/06/14 11:01:25 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/06/14 16:08:55 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PARSER_H
+#define PARSER_H
+
+
+enum e_type_node
+{
+    NODE = 1,
+    PIPE_LINE = 2,
+};
+
+typedef struct commands
+{
+    char **command_args;
+    int type_node;
+    int infile;
+    int outfile;
+    char *path;
+    bool pipe;
+    bool and_;
+    bool or_;
+    bool builtin;
+    struct command* left;
+    struct command* right;
+} command_t;
+
+
+#endif
