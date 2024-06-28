20b87e4ce7414eb240be4e3732e82890879dc63c
nourddine-benyahya <nourddine.benyahya02@gmail.com>
2024-06-28 11:09:45 +0100

    mistak editing

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 /*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 10:16:45 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/06/28 10:17:04 by nbenyahy         ###   ########.fr       */
+/*   Updated: 2024/06/28 11:09:01 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,18 +41,18 @@ int main(int ac, char **av, char  **ev)
         elem = lexer();
         if (!elem)
             continue;
-        t_command *root = parser(elem);
-        print_tree(root, 0);
-        // while (elem)
-        // {
-        //     printf("~%s~                       ", elem->content);
-        //     printf("~%c~                       ", elem->type == WORD ? 'W' : elem->type == AND ? 'A' : elem->type == OR ? 'O' : elem->type == HERE_DOC ? 'H' : elem->type == DREDIR_OUT ? 'D' : elem->type);
-        //     printf("~%s~        \n", elem->state == GENERAL ? "GENERAL" : elem->state == IN_DQUOTE ? "IN DOUBLE QUOTE" : "IN QOUTE");
-        //     elem = elem->next;
-        // }
-        // char *content  = here_doc("$lim");
-        // // printf("here doc content without expanding");
-        // if (content)
-        //     printf("here doc content with expanding : %s\n", expand_here_doc_content(content, env));
+        // t_command *root = parser(elem);
+        // print_tree(root, 0);
+        while (elem)
+        {
+            printf("~%s~                       ", elem->content);
+            printf("~%c~                       ", elem->type == WORD ? 'W' : elem->type == AND ? 'A' : elem->type == OR ? 'O' : elem->type == HERE_DOC ? 'H' : elem->type == DREDIR_OUT ? 'D' : elem->type);
+            printf("~%s~        \n", elem->state == GENERAL ? "GENERAL" : elem->state == IN_DQUOTE ? "IN DOUBLE QUOTE" : "IN QOUTE");
+            elem = elem->next;
+        }
+        char *content  = here_doc("$lim");
+        // printf("here doc content without expanding");
+        if (content)
+            printf("here doc content with expanding : %s\n", expand_here_doc_content(content, env));
     }
 }
