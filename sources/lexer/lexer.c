9a36843c56e878d1100a93ab39a5ff5903618ebe
nourddine-benyahya <nourddine.benyahya02@gmail.com>
2024-07-23 16:42:14 +0100

    norm double qoutes

--- sources/lexer/lexer.c
+++ sources/lexer/lexer.c
@@ -6,21 +6,12 @@
 /*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 15:12:28 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/07/23 15:19:04 by nbenyahy         ###   ########.fr       */
+/*   Updated: 2024/07/23 16:19:30 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "local_lexer.h"
 
-// int	is_token(char c)
-// {
-// 	if (c != WHITE_SPACE && c != '\t' && c != NEW_LINE && c != QOUTE
-// 		&& c != DOUBLE_QUOTE && c != ENV && c != PIPE_LINE && c != REDIR_IN
-// 		&& c != REDIR_OUT && c != START_SUBSHELL && c != END_SUBSHELL
-// 		&& c != WILDCARD)
-// 		return (1);
-// 	return (0);
-// }
 
 int	env_handeler(t_elem **elem, char *line, int *i, int state)
 {
@@ -49,109 +40,47 @@ int	env_handeler(t_elem **elem, char *line, int *i, int state)
 	return (0);
 }
 
-// int	qoute_handler(t_elem **elem, char *line, int *i)
+
+// int	double_qoute_handler(t_elem **elem, char *line, int *i)
 // {
 // 	int	current_index;
 
-// 	if (line[(*i)] == QOUTE)
+// 	if (line[(*i)] == DOUBLE_QUOTE)
 // 	{
-// 		if (allocate_node(elem, ft_strdup("\'"), GENERAL, QOUTE))
+// 		if (allocate_node(elem, ft_strdup("\""), GENERAL, DOUBLE_QUOTE))
 // 			return (1);
 // 		(*i)++;
 // 		current_index = (*i);
 // 	}
-// 	while (line[(*i)] && line[(*i)] != QOUTE)
+// 	while (line[(*i)] && line[(*i)] != DOUBLE_QUOTE)
 // 	{
-// 		while (line[(*i)] && line[(*i)] != QOUTE)
+// 		while (line[(*i)] && line[(*i)] != DOUBLE_QUOTE && !(line[(*i)] == ENV
+// 				&& (ft_isalpha(line[(*i) + 1]) || line[(*i) + 1] == '_'
+// 					|| line[(*i) + 1] == '?')))
 // 			(*i)++;
-// 		;
-// 		if (allocate_node(elem, ft_substr(line, current_index, (*i)
-// 					- current_index), IN_QUOTE, WORD))
-// 			return (1);
+// 		if ((*i) != current_index)
+// 		{
+// 			if (allocate_node(elem, ft_substr(line, current_index, (*i)
+// 						- current_index), IN_DQUOTE, WORD))
+// 				return (1);
+// 		}
+// 		if (line[(*i)] == ENV && (ft_isalpha(line[(*i) + 1]) || line[(*i)
+// 				+ 1] == '_' || line[(*i) + 1] == '?'))
+// 		{
+// 			if (env_handeler(elem, line, i, IN_DQUOTE))
+// 				return (1);
+// 		}
+// 		current_index = (*i);
 // 	}
-// 	if (line[(*i)] == QOUTE)
+// 	if (line[(*i)] == DOUBLE_QUOTE)
 // 	{
-// 		if (allocate_node(elem, ft_strdup("\'"), GENERAL, QOUTE))
+// 		if (allocate_node(elem, ft_strdup("\""), GENERAL, DOUBLE_QUOTE))
 // 			return (1);
 // 		(*i)++;
 // 		return (0);
 // 	}
 // 	else
-//         return (print_err(2, 2,"synthax error : missing a quote\n"), 1);
-// }
-
-int	double_qoute_handler(t_elem **elem, char *line, int *i)
-{
-	int	current_index;
-
-	if (line[(*i)] == DOUBLE_QUOTE)
-	{
-		if (allocate_node(elem, ft_strdup("\""), GENERAL, DOUBLE_QUOTE))
-			return (1);
-		(*i)++;
-		current_index = (*i);
-	}
-	while (line[(*i)] && line[(*i)] != DOUBLE_QUOTE)
-	{
-		while (line[(*i)] && line[(*i)] != DOUBLE_QUOTE && !(line[(*i)] == ENV
-				&& (ft_isalpha(line[(*i) + 1]) || line[(*i) + 1] == '_'
-					|| line[(*i) + 1] == '?')))
-			(*i)++;
-		if ((*i) != current_index)
-		{
-			if (allocate_node(elem, ft_substr(line, current_index, (*i)
-						- current_index), IN_DQUOTE, WORD))
-				return (1);
-		}
-		if (line[(*i)] == ENV && (ft_isalpha(line[(*i) + 1]) || line[(*i)
-				+ 1] == '_' || line[(*i) + 1] == '?'))
-		{
-			if (env_handeler(elem, line, i, IN_DQUOTE))
-				return (1);
-		}
-		current_index = (*i);
-	}
-	if (line[(*i)] == DOUBLE_QUOTE)
-	{
-		if (allocate_node(elem, ft_strdup("\""), GENERAL, DOUBLE_QUOTE))
-			return (1);
-		(*i)++;
-		return (0);
-	}
-	else
-        return (print_err(2, 2,"synthax error : missing a double quote\n"), 1);
-}
-
-// int	general_handler(t_elem **elem, char *line, int *i, int *subshell)
-// {
-// 	int	current_index;
-
-// 	current_index = (*i);
-// 	while (line[(*i)] && (line[(*i)] != QOUTE && line[(*i)] != DOUBLE_QUOTE))
-// 	{
-// 		while (((line[(*i)] && (is_token(line[(*i)]) && !(line[(*i)] == '&'
-// 							&& line[(*i) + 1] == '&'))) || (line[(*i)] == ENV
-// 					&& !ft_isalpha(line[(*i) + 1]) && line[(*i) + 1] != '_'
-// 					&& line[(*i) + 1] != '?')))
-// 			(*i)++;
-// 		if (current_index != (*i))
-// 		{
-// 			if (allocate_node(elem, ft_substr(line, current_index, (*i)
-// 						- current_index), GENERAL, WORD))
-// 				return (1);
-// 			current_index = (*i);
-// 		}
-// 		if (line[(*i)] == ENV && (ft_isalpha(line[(*i) + 1]) || line[(*i)
-// 				+ 1] == '_' || line[(*i) + 1] == '?'))
-// 		{
-// 			if (env_handeler(elem, line, i, GENERAL))
-// 				return (1);
-// 		}
-// 		else if (general_tokens(line, elem, i, subshell))
-// 			return (1);
-// 		current_index = (*i);
-// 	}
-// 	return (0);
+//         return (print_err(2, 2,"synthax error : missing a double quote\n"), 1);
 // }
 
 t_elem	*tokenize(char *line, int *subshell, t_elem **elem)
--- sources/lexer/local_lexer.h
+++ sources/lexer/local_lexer.h
@@ -6,7 +6,7 @@
 /*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 16:06:17 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/07/23 15:30:10 by nbenyahy         ###   ########.fr       */
+/*   Updated: 2024/07/23 16:38:08 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void free_elem(t_elem *elem);
 int allocate_node(t_elem **elem, char *content, int state, int token);
 int	env_handeler(t_elem **elem, char *line, int *i, int state);
 t_list    *syntax_error(t_elem *elem);
+int	double_qoute_handler(t_elem **elem, char *line, int *i);
 int	qoute_handler(t_elem **elem, char *line, int *i);
 int	general_handler(t_elem **elem, char *line, int *i, int *subshell);
 #endif
