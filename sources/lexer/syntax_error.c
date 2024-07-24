1449a17848262d1b8fe2a6c98dfe2db2f4d1a31b
nourddine-benyahya <nourddine.benyahya02@gmail.com>
2024-07-24 09:47:57 +0100

    other syntax

--- sources/lexer/syntax_error.c
+++ sources/lexer/syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 09:43:39 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/07/24 09:20:50 by nbenyahy         ###   ########.fr       */
+/*   Updated: 2024/07/24 09:46:00 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,10 +66,58 @@ t_list *here_doc_syntax(t_elem *elem, t_list **list, t_list **original)
     return (NULL);
 }
 
+int subshell_syntax(t_elem **elem)
+{
+    if (*elem && (*elem)->type == START_SUBSHELL)
+    {
+        *elem = (*elem)->next;
+        while (elem && (*elem)->type == WHITE_SPACE)
+            *elem = (*elem)->next;
+        if (!*elem || (*elem)->type == END_SUBSHELL || (*elem)->type == AND || (*elem)->type == OR || (*elem)->type == PIPE_LINE)
+            return (0);
+        return (1);
+    }
+    else if ((*elem) && (*elem)->type == END_SUBSHELL)
+    {
+        *elem = (*elem)->next;
+        while (*elem && (*elem)->type == WHITE_SPACE)
+            *elem = (*elem)->next;
+        if (!*elem)
+            return (1);
+        if ((*elem)->type == WORD)
+            return (0);
+        if (*elem && (((*elem)->type != END_SUBSHELL && (*elem)->type != AND && (*elem)->type != OR && (*elem)->type != PIPE_LINE && (*elem)->type != REDIR_IN && (*elem)->type != REDIR_OUT) || (*elem)->type == START_SUBSHELL))
+            return (0);
+    }
+    return (2);
+}
+
+int other_syntax(t_elem **elem)
+{
+    if ((*elem) && ((*elem)->type == AND || (*elem)->type == OR || (*elem)->type == PIPE_LINE))
+    {
+        (*elem) = (*elem)->next;
+        while ((*elem) && (*elem)->type == WHITE_SPACE)
+            (*elem) = (*elem)->next;
+        if (!(*elem) || ((*elem)->type != REDIR_IN && (*elem)->type != REDIR_OUT 
+            && (*elem)->type != DREDIR_OUT && (*elem)->type != HERE_DOC && (*elem)->type != WORD && (*elem)->type != ENV && (*elem)->type != START_SUBSHELL && (*elem)->type != QOUTE && (*elem)->type != DOUBLE_QUOTE))
+                return (0);
+    }
+    else if ((*elem) && ((*elem)->type == REDIR_IN || (*elem)->type == REDIR_OUT 
+            || (*elem)->type == DREDIR_OUT))
+    {
+        (*elem) = (*elem)->next;
+        while ((*elem) && (*elem)->type == WHITE_SPACE)
+            (*elem) = (*elem)->next;
+        if (!(*elem) || ((*elem)->type != WORD && (*elem)->type != ENV && (*elem)->type != QOUTE && (*elem)->type != DOUBLE_QUOTE && (*elem)->type != WILDCARD))
+            return (0);
+    }
+    return (1);
+}
+
 t_list    *syntax_error(t_elem *elem)
 {
     int i;
-    // t_elem *tmp;
     t_list *list;
     t_list *original;
 
@@ -94,86 +142,42 @@ t_list    *syntax_error(t_elem *elem)
             if (tmp5 && tmp5->type == START_SUBSHELL)
                 return (original);
         }
-        if (elem && elem->type == START_SUBSHELL)
+        if (elem && (elem->type == START_SUBSHELL || elem->type == END_SUBSHELL))
         {
-            elem = elem->next;
-            while (elem && elem->type == WHITE_SPACE)
-                elem = elem->next;
-            if (!elem || elem->type == END_SUBSHELL || elem->type == AND || elem->type == OR || elem->type == PIPE_LINE)
+            int a = subshell_syntax(&elem);
+            if (a == 0)
                 return (original);
-            continue;
-        }
-        else if (elem && elem->type == END_SUBSHELL)
-        {
-            elem = elem->next;
-            while (elem && elem->type == WHITE_SPACE)
-                elem = elem->next;
-            if (!elem)
+            else if (a == 1)
                 continue;
-            if (elem->type == WORD)
-                return (original);
-            if (elem && ((elem->type != END_SUBSHELL && elem->type != AND && elem->type != OR && elem->type != PIPE_LINE && elem->type != REDIR_IN && elem->type != REDIR_OUT) || elem->type == START_SUBSHELL))
-                return (original);
         }
         else if (elem && elem->type == HERE_DOC)
         {
             if (here_doc_syntax(elem, &list, &original) != NULL)
                 return (original);
-            // tmp = elem;
-            // if (tmp)
-            //     tmp = tmp->next;
-            // while (tmp && tmp->type == WHITE_SPACE)
-            //     tmp = tmp->next;
-            // if (tmp && !is_tocken(tmp))
-            // {
-            //     if (tmp && (tmp->type == QOUTE || tmp->type == DOUBLE_QUOTE))
-            //     {
-            //         tmp = tmp->next;
-            //         while (tmp && (tmp->type != QOUTE || tmp->type != DOUBLE_QUOTE))
-            //         {
-            //             ft_lstadd_back(&list, ft_lstnew(ft_strdup(tmp->content)));
-            //             tmp = tmp->next;
-            //         }
-            //         char *str;
-            //         str = calloc(2, 1);
-            //         char *tmp_str;
-            //         while (list)
-            //         {
-            //             tmp_str = ft_strjoin(str, list->content);
-            //             free(str);
-            //             str = tmp_str;
-            //             list = list->next;
-            //         }
-            //         ft_lstadd_back(&original, ft_lstnew(str));
-            //         elem = elem->next;
-            //     }
-            //     else if (tmp && (tmp->type == WORD || tmp->type != ENV))
-            //     {
-            //         ft_lstadd_back(&original, ft_lstnew(tmp->content));
-            //         elem = elem->next;
-            //     }
-            // }
-            // else
-            //     return (original);
         }
-        if (elem && (elem->type == AND || elem->type == OR || elem->type == PIPE_LINE))
+        if (elem && ((elem->type == AND || elem->type == OR || elem->type == PIPE_LINE) || (elem->type == REDIR_IN || elem->type == REDIR_OUT || elem->type == DREDIR_OUT)))
         {
-            elem = elem->next;
-            while (elem && elem->type == WHITE_SPACE)
-                elem = elem->next;
-            if (!elem || (elem->type != REDIR_IN && elem->type != REDIR_OUT 
-                && elem->type != DREDIR_OUT && elem->type != HERE_DOC && elem->type != WORD && elem->type != ENV && elem->type != START_SUBSHELL && elem->type != QOUTE && elem->type != DOUBLE_QUOTE))
-                    return (original);
-        }
-        else if (elem && (elem->type == REDIR_IN || elem->type == REDIR_OUT 
-                || elem->type == DREDIR_OUT))
-        {
-            elem = elem->next;
-            while (elem && elem->type == WHITE_SPACE)
-                elem = elem->next;
-            if (!elem || (elem->type != WORD && elem->type != ENV && elem->type != QOUTE && elem->type != DOUBLE_QUOTE && elem->type != WILDCARD))
+            if (other_syntax(&elem) == 0)
                 return (original);
         }
+        // if (elem && (elem->type == AND || elem->type == OR || elem->type == PIPE_LINE))
+        // {
+        //     elem = elem->next;
+        //     while (elem && elem->type == WHITE_SPACE)
+        //         elem = elem->next;
+        //     if (!elem || (elem->type != REDIR_IN && elem->type != REDIR_OUT 
+        //         && elem->type != DREDIR_OUT && elem->type != HERE_DOC && elem->type != WORD && elem->type != ENV && elem->type != START_SUBSHELL && elem->type != QOUTE && elem->type != DOUBLE_QUOTE))
+        //             return (original);
+        // }
+        // else if (elem && (elem->type == REDIR_IN || elem->type == REDIR_OUT 
+        //         || elem->type == DREDIR_OUT))
+        // {
+        //     elem = elem->next;
+        //     while (elem && elem->type == WHITE_SPACE)
+        //         elem = elem->next;
+        //     if (!elem || (elem->type != WORD && elem->type != ENV && elem->type != QOUTE && elem->type != DOUBLE_QUOTE && elem->type != WILDCARD))
+        //         return (original);
+        // }
         else if (elem)
             elem = elem->next;
     }
