f46bcb20573c5e7841249ddc5416751175ea2ec4
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-07-26 10:28:43 +0100

    up leaks

--- sources/helper/print_err.c
+++ sources/helper/print_err.c
@@ -6,7 +6,7 @@
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 12:27:44 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/07/24 15:12:28 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/07/25 12:12:43 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	print_err(int count, ...)
 	ft_putstr_fd(RED, 2);
 	while (i < count)
 	{
-		printf ("%d\n", i);
+		// printf ("%d\n", i);
 		str = va_arg(args, char *);
 		ft_putstr_fd(str, 2);
 		i++;
